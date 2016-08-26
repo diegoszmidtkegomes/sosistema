7d209fcce6e2ba2031b4c23d35f45f992d9ea51f
0300152 <0300152@W000204>
2016-08-25 21:17:01 -0300

    Renomeado classe e package

--- sosistema/src/br/ifrs/so/Main.java
+++ sosistema/src/br/ifrs/so/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sosistema;
+package br.ifrs.so;
 
 /**
  *
  * @author 0300152
  */
-public class Sosistema {
+public class Main {
 
     /**
      * @param args the command line arguments
